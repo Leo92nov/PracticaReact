05398309ee44770e120b0d3b7135a3597902ecb6
Leonardo Nonino <lnonino92@gmail.com>
2026-01-04 18:15:01 -0300

    siguiendo el proyecto

--- src/components/ProductoComponents.jsx
+++ src/components/ProductoComponents.jsx
@@ -1,27 +1,26 @@
 import { useState } from "react"
 
-export default function ProductoComponent({fuente, titulo, precio, cantidad}){
+export default function ProductoComponent({fuente, titulo, precio}){
 
-    const [contador, setContador] = useState(0);
+    const [contador, setContador,] = useState(0);
     const handleClick = () =>{
         setContador (contador + 1)
     }
-
-    const [restar, setRestar] = useState(0);
-    const handleClick2 = () =>{
-        setRestar (restar - 1)
+    const handleMinus = () => {
+        setContador (contador - 1)
     }
 
+    
 
     return <>
-    <div className="flex items-center w-[30%] flex-col" >
-        <img  className="w-[500px] h-[500px]" src={fuente} alt="" />
+    <div className="flex items-center w-[33%] flex-col flex-wrap mt-8" >
+        <img  className="w-[300px] h-[300px]" src={fuente} alt="" />
         <h3>{titulo}</h3>
-        <h3>{precio}</h3>
-        <div>
-            <button onClick={handleClick2}>-</button>
-            <p>{cantidad}</p>
-            <button onClick={handleClick}>+</button>
+        <h3>$ {precio}</h3>
+        <div className="flex gap-2 items-center bg-red-100">
+            <button className="border rounded-md border-black-2 bg-blue-200 text-xl w-6 h-6 flex items-center justify-center pb-1 pr-0.5 cursor-pointer" onClick={handleMinus}>-</button>
+            <p className="text-xl p-1">{contador}</p>
+            <button className="border rounded-md border-black-2 bg-blue-200 text-xl w-6 h-6 flex items-center justify-center pb-1 pr-0.5 cursor-pointer" onClick={handleClick}>+</button>
         </div>
     </div>
     
--- src/components/ProductosContenedor.jsx
+++ src/components/ProductosContenedor.jsx
@@ -3,9 +3,13 @@ import ProductoComponent from "./ProductoComponents";
 
 export default function ProductosContenedor(){
     return <>
-    <ProductoComponent fuente="../../Media/imgs/4060.png"  titulo="Nvidia 3060 RTX" precio="600" cantidad="0"></ProductoComponent>
-    <ProductoComponent></ProductoComponent>
-    <ProductoComponent></ProductoComponent>
-    
+    <div className="flex flex-wrap">
+    <ProductoComponent fuente="../../Media/imgs/3060.jpg"  titulo="Nvidia 3060 RTX" precio="600" cantidad="0"></ProductoComponent>
+    <ProductoComponent fuente="../../Media/imgs/4060.png"  titulo="Nvidia 4060 RTX" precio="800" cantidad="0"></ProductoComponent>
+    <ProductoComponent fuente="../../Media/imgs/5060.jpg"  titulo="Nvidia 5060 RTX" precio="900" cantidad="0"></ProductoComponent>
+    <ProductoComponent fuente="../../Media/imgs/5060.jpg"  titulo="Nvidia 5060 RTX" precio="900" cantidad="0"></ProductoComponent>
+    <ProductoComponent fuente="../../Media/imgs/5060.jpg"  titulo="Nvidia 5060 RTX" precio="900" cantidad="0"></ProductoComponent>
+    <ProductoComponent fuente="../../Media/imgs/5060.jpg"  titulo="Nvidia 5060 RTX" precio="900" cantidad="0"></ProductoComponent>
+    </div>
     </>
 }
